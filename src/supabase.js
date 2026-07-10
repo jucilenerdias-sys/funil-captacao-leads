c1b9fb0493824570c2202f3be55cfb741d24ea89
Jucilene Rodrigues <jucilene.r.dias@gmail.com>
2026-07-10 11:29:18 -0300

    Conecta o funil ao novo banco de dados seguro

--- src/supabase.js
+++ src/supabase.js
@@ -1,9 +1,9 @@
 import { createClient } from '@supabase/supabase-js';
 
 // Coloque o seu link verdadeiro do Supabase entre as aspas abaixo:
-const supabaseUrl = "https://aeygpbcmypouyhrcjoam.supabase.co";
+const supabaseUrl = "https://obsrcrfstefjnxseqbmh.supabase.co/rest/v1/";
 
 // Coloque a sua chave enorme do Supabase entre as aspas abaixo:
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFleWdwYmNteXBvdXlocmNqb2FtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc3NDkyMzIsImV4cCI6MjA5MzMyNTIzMn0.WfhmiNUda3FG5yUnGrYC53gdTzCmkCx4K0QIH0y9B5E";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9ic3JjcmZzdGVmam54c2VxYm1oIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM2OTAwODgsImV4cCI6MjA5OTI2NjA4OH0.pwOH1PDt3zBCDWvfn7vblyT2ceeIjKrROvMRB1c7C78";
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
